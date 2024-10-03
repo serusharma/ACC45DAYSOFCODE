9e4033d37c6c1eb24083907c5d42894c95e7c242
serusharma <sharmasachin80505@gmail.com>
2024-10-04 00:00:00 +0530

    Add files via upload

---
+++ MINPIZZA.java
@@ -0,0 +1,16 @@
+import java.util.*;
+import java.lang.*;
+import java.io.*;
+
+class Codechef
+{
+	public static void main (String[] args) throws java.lang.Exception
+	{
+	    int a = 2;
+	    int b = 3;
+	    int c = 77;
+	    int d = 66;
+		// your code goes here
+
+	}
+}
